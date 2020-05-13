dc92bd48ebf5b04c7f2e222ad2a18b65981932e6
Martin Beckmann <m.beckmann@konkret-mafo.cloud>
2020-05-13 04:39:14 +0200

    neelz

--- app/assets/javascripts/cobrowsing.js
+++ app/assets/javascripts/cobrowsing.js
@@ -119,43 +119,43 @@ let stopCoBrowsing = function(){
 
 function toggle_iframes_1(){
   $('#external_viewport_2')
-      .css('pointer-events', 'all')
+      //.css('pointer-events', 'all')
       .animate({opacity: 1.0}, 700, function () {
-        $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+        //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
       });
   $('#external_viewport_1').animate({opacity: 0.0}, 2750, function () {
-    $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+    //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
   });
-  $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+  //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
 }
 
 function toggle_iframes_2(){
   $('#external_viewport_2')
-      .css('pointer-events', 'none')
+      //.css('pointer-events', 'none')
       .animate({opacity: 0.0}, 2750, function () {
-        $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+        //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
       });
   $('#external_viewport_1').animate({opacity: 1.0}, 700, function () {
-    $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+    //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
   });
-  $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+  //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
 }
 
 let refreshCoBrowsing = function () {
   if (coBrowsingState.active) {
-    coBrowsingState.scrollTop = $('#inside_external_frame_iframe_container').scrollTop();
+    //coBrowsingState.scrollTop = $('#inside_external_frame_iframe_container').scrollTop();
     if (coBrowsingState.activeIFrame === 0) {
       $('#external_viewport_2')
-          //.attr('src', '')
+          .attr('src', '')
           .attr('src', coBrowsingState.url);
-      $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+      //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
       setTimeout(toggle_iframes_1, 2500);
       coBrowsingState.activeIFrame = 1;
     } else {
       $('#external_viewport_1')
-          //.attr('src', '')
+          .attr('src', '')
           .attr('src', coBrowsingState.url);
-      $('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
+      //$('#inside_external_frame_iframe_container').scrollTop(coBrowsingState.scrollTop);
       setTimeout(toggle_iframes_2, 2500);
       coBrowsingState.activeIFrame = 0;
     }
