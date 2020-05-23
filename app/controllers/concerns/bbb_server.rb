81c3ec13e22d6bb9e1a1d8fd414f08e0c55cc4ac
Martin Beckmann <m.beckmann@konkret-mafo.cloud>
2020-05-23 21:19:13 +0200

    https://github.com/bigbluebutton/bigbluebutton/pull/8575

--- app/controllers/concerns/bbb_server.rb
+++ app/controllers/concerns/bbb_server.rb
@@ -56,6 +56,8 @@ module BbbServer
     join_opts[:join_via_html5] = true
     join_opts[:guest] = true if options[:require_moderator_approval] && !options[:user_is_moderator]
 
+    join_opts["userdata-bbb_show_participants_on_login"] = false if room.is_neelz_room?
+
     bbb_server.join_meeting_url(room.bbb_id, name, password, join_opts)
   end
 
@@ -71,7 +73,8 @@ module BbbServer
     join_opts[:userID] = uid if uid
     join_opts[:join_via_html5] = true
     join_opts[:guest] = true if options[:require_moderator_approval] && !options[:user_is_moderator]
-    #join_opts[:redirect] = "FALSE"
+
+    join_opts["userdata-bbb_show_participants_on_login"] = false if room.is_neelz_room?
 
     bbb_server.join_meeting_url(room.bbb_id, name, password, join_opts)
 
